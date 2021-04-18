9768f8989a2873d57a1dc14445f10ee2d04fba40
Anton Baric <anton.baric99@gmail.com>
2021-04-18 21:08:42 +0200

    Second commit

---
+++ src/test/java/com/killstealer/KillStealerLoLApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killstealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillStealerLoLApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
