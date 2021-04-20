4e3a44e16a17bd672197010b3966bc1b9d0d70ba
Anton Baric <anton.baric99@gmail.com>
2021-04-20 14:57:24 +0200

    Added getUserById method

--- src/main/java/com/killstealer/controllers/UserController.java
+++ src/main/java/com/killstealer/controllers/UserController.java
@@ -3,10 +3,7 @@ package com.killstealer.controllers;
 import com.killstealer.entities.User;
 import com.killstealer.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +18,8 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable long id) {
+        return userService.getUserById(id);
+    }
 }
--- src/main/java/com/killstealer/services/UserService.java
+++ src/main/java/com/killstealer/services/UserService.java
@@ -4,10 +4,10 @@ import com.killstealer.configs.MyUserDetailsService;
 import com.killstealer.entities.User;
 import com.killstealer.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,5 +20,9 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepo.findAll();
     }
-    
+
+    public User getUserById(long id) {
+        Optional<User> user = userRepo.findById(id);
+        return user.orElse(null);
+    }
 }
