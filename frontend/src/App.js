d213fd292a5ef489ba4e03c31b4a7a65d7b78a49
Anton Baric <anton.baric99@gmail.com>
2021-10-17 14:51:57 +0200

    added challenger league

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import Summoner from './components/Summoner'
+import Challenger from './components/Challenger'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/summoner/:summonerName">
-          <Summoner />
-        </Route>
-      </Switch>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/challenger">
+            <Challenger />
+          </Route>
+          <Route exact path="/summoner/:summonerName">
+            <Summoner />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
     <div>
-      <input type="text"></input>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/challenger">Challenger</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
